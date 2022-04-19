306c578b76c96e60a283c5819f188658e69e1648
cory <quinlanc@sonoma.edu>
2022-04-19 00:07:45 -0700

    removed a bit

--- nft-keychain/src/App.js
+++ nft-keychain/src/App.js
@@ -3,8 +3,8 @@ import MainDrawer from './menu/MainDrawer';
 
 export default function App({user, logoutAction}) {
   const mainPageTitle = "title?";
-  const [button1, setButton1] = useState("first button")
-  const [button2, setButton2] = useState("second button")
+  const button1 = "Collection";
+  const button2 = "Settings";
 
   return (
       <Fragment>
