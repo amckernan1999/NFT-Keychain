2f2d087df2be6ab6d48d729673b207af6e960010
Andrew McKernan <Amckernan19990@gmail.com>
2022-05-01 18:27:50 -0700

    database insert for collections w/ 15 images

--- nft-keychain-db-cmds/inserttestcollections_nftkeychain.sql
+++ nft-keychain-db-cmds/inserttestcollections_nftkeychain.sql
@@ -1,15 +1,29 @@
-INSERT INTO collections (id, url, title, path, userID)
-	VALUES ('1','google.com/nftImage1','Bored Ape #50','opeansea.com/nft-image1','1');
+INSERT INTO collections (id, url, title, path, userID)  VALUES ('1','https://lh3.googleusercontent.com/n1vL9X7Rk1m4tBcDUVKCVqqthyTRXoSuxTB-018tn_-slE5TgLKs5gyTYJIhVlKhnBQd8vi2fhlJyu1udfb9G7h0G6HlrDW9zAclsQ=w600','CyberKong #924','https://lh3.googleusercontent.com/n1vL9X7Rk1m4tBcDUVKCVqqthyTRXoSuxTB-018tn_-slE5TgLKs5gyTYJIhVlKhnBQd8vi2fhlJyu1udfb9G7h0G6HlrDW9zAclsQ=w600','1');
 
-INSERT INTO collections (id, url, title, path, userID)
-	VALUES ('2','google.com/nftImage2','Funny Cat #4728','opeansea.com/nft-image2','1');
+INSERT INTO collections (id, url, title, path, userID)  VALUES ('2','https://lh3.googleusercontent.com/2MvF-ld9MHcehtT77gsZ7d-QYYRB5e5XEcO2F0HcwRGeIgansbFxNZUgAWgsuBKWT0EuCRojN9aRLqf-1-i9YAyW211tS7pYzzhWjQ=w600','Okay Bear #4215','https://lh3.googleusercontent.com/2MvF-ld9MHcehtT77gsZ7d-QYYRB5e5XEcO2F0HcwRGeIgansbFxNZUgAWgsuBKWT0EuCRojN9aRLqf-1-i9YAyW211tS7pYzzhWjQ=w600','1');
 
-INSERT INTO collections (id, url, title, path, userID)
-	VALUES ('3','google.com/nftImage3','Guy w/ cowboy hat #75','opeansea.com/nft-image3','1');
+INSERT INTO collections (id, url, title, path, userID)  VALUES ('3','https://lh3.googleusercontent.com/Pvdc_F0Ri1GAaHhf_dmodBkAmqZd4FO34LrOuUTeaUJ8okJLXgG-YRKefgsxzIP2qudMgUCsb4y7idnCUT7IgDdY9HP8fAQMi_Hw=w600','Moonbirds #1411','https://lh3.googleusercontent.com/Pvdc_F0Ri1GAaHhf_dmodBkAmqZd4FO34LrOuUTeaUJ8okJLXgG-YRKefgsxzIP2qudMgUCsb4y7idnCUT7IgDdY9HP8fAQMi_Hw=w600','1');
 
-INSERT INTO collections (id, url, title, path, userID)
-	VALUES ('4','google.com/nftImage4','Bored Ape #71 ','opeansea.com/nft-image4','1');
+INSERT INTO collections (id, url, title, path, userID)  VALUES ('4','https://lh3.googleusercontent.com/LP4bWYE8PliPxmMt3b_2cUXD4oxdjF-tP9SFByWr4d55BKZ9pr4iswTVmejMufwMar44jCWBm40MiBnF1oKjcYTSrG23hVYcpUoHGg=w600','Sufficient Shrimp #16530','https://lh3.googleusercontent.com/LP4bWYE8PliPxmMt3b_2cUXD4oxdjF-tP9SFByWr4d55BKZ9pr4iswTVmejMufwMar44jCWBm40MiBnF1oKjcYTSrG23hVYcpUoHGg=w600','1');
 
-INSERT INTO collections (id, url, title, path, userID)
-	VALUES ('5','google.com/nftImage5','Not User 001 property #50','opeansea.com/nft-image5','2');
+INSERT INTO collections (id, url, title, path, userID)  VALUES ('5','https://lh3.googleusercontent.com/xNki4DddWaRXGMd1tZ9mM3TmiH0mVrwKAOUzhOxsLns-ecA_S7bwu8Ot3Z6MrQgkIfvxGyGMVbcVBTZS4MHzJzfd3uffnaYiKTCqzg=w600','Akutar #7199','https://lh3.googleusercontent.com/xNki4DddWaRXGMd1tZ9mM3TmiH0mVrwKAOUzhOxsLns-ecA_S7bwu8Ot3Z6MrQgkIfvxGyGMVbcVBTZS4MHzJzfd3uffnaYiKTCqzg=w600','1');
 
+INSERT INTO collections (id, url, title, path, userID)  VALUES ('6','https://lh3.googleusercontent.com/VWDOkxLNQagqCnZ6VpTzKbuJVHCtuy7xfwFgo-lNqOZhyuIxxym5b4iGgPiWBgqQCEYOtBaKPthhRoFNxkBQrPy-SWoVjHmLqsEYIw=w600','Meebit #16284','https://lh3.googleusercontent.com/VWDOkxLNQagqCnZ6VpTzKbuJVHCtuy7xfwFgo-lNqOZhyuIxxym5b4iGgPiWBgqQCEYOtBaKPthhRoFNxkBQrPy-SWoVjHmLqsEYIw=w600','1');
+
+INSERT INTO collections (id, url, title, path, userID)  VALUES ('7','https://lh3.googleusercontent.com/OAzxXcaL57fxBIGMj8BeoiuXPx6ZQ3m40EcF7x8aZShu5RD8pdCMITYoGkINIqVuTmcrXedsp16DMKwTZC1K1tLOENzjJb0lrnbf=w600','Doodle #491','https://lh3.googleusercontent.com/OAzxXcaL57fxBIGMj8BeoiuXPx6ZQ3m40EcF7x8aZShu5RD8pdCMITYoGkINIqVuTmcrXedsp16DMKwTZC1K1tLOENzjJb0lrnbf=w600','1');
+
+INSERT INTO collections (id, url, title, path, userID)  VALUES ('8','https://lh3.googleusercontent.com/yAGJQzpyEicd1gkl15dzOWj8SbVvQ2flAIjzrrnrnrg7-5J4pDMmxjwUWke2B0A5xsEx0LfMCyRjavECQdAoz_8DW4SZW9cfUBYi=w600','CloneX #15925','https://lh3.googleusercontent.com/yAGJQzpyEicd1gkl15dzOWj8SbVvQ2flAIjzrrnrnrg7-5J4pDMmxjwUWke2B0A5xsEx0LfMCyRjavECQdAoz_8DW4SZW9cfUBYi=w600','1');
+
+INSERT INTO collections (id, url, title, path, userID)  VALUES ('9','https://lh3.googleusercontent.com/Dxq5D7bi-zqf0pXt9WqowgOFfE2k2BfkS8A-aILbBU1jWyYNcdZ_25iD_P69Klu72qKDUMGXG-IDoG42D4p1a061Ag5UfCK0IwcNrw=w600','Bored Ape Kennel Club#1884','https://lh3.googleusercontent.com/Dxq5D7bi-zqf0pXt9WqowgOFfE2k2BfkS8A-aILbBU1jWyYNcdZ_25iD_P69Klu72qKDUMGXG-IDoG42D4p1a061Ag5UfCK0IwcNrw=w600','1');
+
+INSERT INTO collections (id, url, title, path, userID)  VALUES ('10','https://lh3.googleusercontent.com/x1rCSUMgCbQ0pOWoNzWv3Bc4gEfL1kcNWTPcmkHd2r0_Ojg2_QcycsVM6Rx1SiN_-AlV7_lhR_QopHumbdelTCh4=w600','CryptoPunk #2667','https://lh3.googleusercontent.com/x1rCSUMgCbQ0pOWoNzWv3Bc4gEfL1kcNWTPcmkHd2r0_Ojg2_QcycsVM6Rx1SiN_-AlV7_lhR_QopHumbdelTCh4=w600','1');
+
+INSERT INTO collections (id, url, title, path, userID)  VALUES ('11','https://lh3.googleusercontent.com/3RudNiWTS11qHas-8LP4WVx4YIqR24AM9Tr_SDGIFGHYe6UmfuLces59dYDfGZif542-6j5WrooqLXPUVOThgc5u--uv4-4AkUCgiQ=w600','Azuki #6916','https://lh3.googleusercontent.com/3RudNiWTS11qHas-8LP4WVx4YIqR24AM9Tr_SDGIFGHYe6UmfuLces59dYDfGZif542-6j5WrooqLXPUVOThgc5u--uv4-4AkUCgiQ=w600','1');
+
+INSERT INTO collections (id, url, title, path, userID)  VALUES ('12','https://lh3.googleusercontent.com/dy1GPB-pZHgb5n5DnP5A1Mv4wX55bcDLimu9kMVg1slAtE62oM_gXywAiPeu101FKOJ3_v6TCWnT72AP3TNTv5iueo1LiXxWLz-YBQ=w600','M1 Mutant Serum','https://lh3.googleusercontent.com/dy1GPB-pZHgb5n5DnP5A1Mv4wX55bcDLimu9kMVg1slAtE62oM_gXywAiPeu101FKOJ3_v6TCWnT72AP3TNTv5iueo1LiXxWLz-YBQ=w600','1');
+
+INSERT INTO collections (id, url, title, path, userID)  VALUES ('13','https://lh3.googleusercontent.com/kYKzOZWmU2NxYS7CRFB4uvO3SjdZmdRlJJ-PT336wSq_fUm6-Wpp8RjXglRma91_rmkZy6p-dGIgDtD9DsPHY_eDH32EgyJpy0fJtw=w600','Cool Cat #8637','https://lh3.googleusercontent.com/kYKzOZWmU2NxYS7CRFB4uvO3SjdZmdRlJJ-PT336wSq_fUm6-Wpp8RjXglRma91_rmkZy6p-dGIgDtD9DsPHY_eDH32EgyJpy0fJtw=w600','1');
+
+INSERT INTO collections (id, url, title, path, userID)  VALUES ('14','https://lh3.googleusercontent.com/CtqJTCTboOzTwmV2AlBuIj6n62kyNGSYH3IMvGs4eo5tcDvBvCPKm1tc1sSyqPyGkhr8SbCsN8Y4R92iuZGd6SaRaEPacfVyjVR0SQ=w600','EightBit #8303','https://lh3.googleusercontent.com/CtqJTCTboOzTwmV2AlBuIj6n62kyNGSYH3IMvGs4eo5tcDvBvCPKm1tc1sSyqPyGkhr8SbCsN8Y4R92iuZGd6SaRaEPacfVyjVR0SQ=w600','1');
+
+INSERT INTO collections (id, url, title, path, userID)  VALUES ('15','https://lh3.googleusercontent.com/cqywHUutEEvePQWtM7svAKRHgv6ohVUkgQmkHS8rsHMLYCLfJ3WN4R_NKcivnnV8J6pksn1Etc9V1Nn1YqkX8ilKyPINg5qz65Dikg=w600','DeGod #8457','https://lh3.googleusercontent.com/cqywHUutEEvePQWtM7svAKRHgv6ohVUkgQmkHS8rsHMLYCLfJ3WN4R_NKcivnnV8J6pksn1Etc9V1Nn1YqkX8ilKyPINg5qz65Dikg=w600','1');
