668a99e4b00de047561b41fd7b2bcf742943b6d3
Andrew McKernan <Amckernan19990@gmail.com>
2022-05-01 17:43:24 -0700

    datagrid/images merged with db functionality

--- nft-keychain/src/pages/collections.js
+++ nft-keychain/src/pages/collections.js
@@ -1,61 +1,66 @@
-import React, {Fragment, useState, useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
-import Typography from "@mui/material/Typography";
+import '../styles.css';
 import API from "../API_Interface/API_Interface";
-
-
-
-
+import Modal from '../Modal';
 
 function Collections({Logout, SelectDevice, Transfer, error, userID}) {
+    console.log('collection userID:', userID);
 
+    const [isOpen, setIsOpen] = React.useState(false);
     const [collection,setCollection] = useState([]);
 
 
     const submitHandler = e => {
         e.preventDefault();
         Logout();
-    }
+    };
 
-    const getKeyButton = (params) => {
+    const renderGetKeyButton = (params) => {
         return (
             <strong>
                 <Button
                     variant="contained"
                     color="primary"
-                    size="small"
-                    style={{ marginLeft: 16 }}
+                    size="medium"
                     onClick={() => {
-                        console.log(params.row.col6);
+                        navigator.clipboard.writeText("Hello").then(r => {console.log('Copied key to clipboard: ', "hello");});
                     }}
-                >
-                    Get Key
+                >GET KEY
                 </Button>
             </strong>
         )
     }
 
     const columns = [
-        {field: 'id', headerName: 'ID', width: 90},
+        {field: 'id', headerName: '', width: '50', disableClickEventBubbling: true,},
+        {field: 'url', headerName: '', width: 240, height: 240, disableColumnFilter: true, disableClickEventBubbling: true,
+            renderCell: (params)=>{
+                return (
+                    <div>
+                        <img src={params.value} alt="nft" className="nft" />
+                    </div>
+                )}
+            },
         {
             field: 'title',
             headerName: 'Title',
+            headerAlign: 'center',
             width: 300,
+            disableClickEventBubbling: true,
         },
         {
-            field: 'url',
-            headerName: 'URL',
-            width: 300,
+            field: 'nftKey',
+            headerName: 'Key',
+            width: 100,
+            headerAlign: 'center',
+            align: 'center',
+            disableColumnFilter: true,
+            disableClickEventBubbling: true,
+            renderCell: renderGetKeyButton,
         },
-        {   field:"lol",
-            type: "buttons",
-            buttons: [{
-                name: "save",
-                cssClass: "my-class"
-            }],
-        }
     ];
 
     useEffect(() => {
@@ -71,13 +76,16 @@ function Collections({Logout, SelectDevice, Transfer, error, userID}) {
 
 
     return (
-        <Fragment>
+        <div style={{height: '100%', width: '100%'}}>
             <Box display="flex" justifyContent="space-evenly" flexDirection="row" alignItems="center" width="100%" mt={2} top={"0"} position={"absolute"}>
                 <Button
                     variant="contained"
                     size="large"
+                    onClick={() => setIsOpen(true)}
                 >ADD NFT
                 </Button>
+                <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+                </Modal>
                 <Button
                     variant="contained"
                     size="large"
@@ -96,16 +104,23 @@ function Collections({Logout, SelectDevice, Transfer, error, userID}) {
                 >LOGOUT
                 </Button>
             </Box>
-            <DataGrid
-
-                rows={collection}
-                columns={columns}
-                pageSize={10}
-                autoHeight={true}
-                checkboxSelection
-                disableSelectionOnClick
-            />
-        </Fragment>
+            <div style={{height: "91%", width: "100%", display: 'flex'}}>
+                <div style={{flexGrow: '1'}}>
+                    <DataGrid
+                        sx={{mt: '4.3%'}}
+                        rowHeight={240}
+                        rows={collection}
+                        rowsPerPageOptions={[5, 10, 25, 100]}
+                        columns={columns}
+                        autoHeight={false}
+                        headerHeight={35}
+                        checkboxSelection
+                        disableSelectionOnClick
+                        pagination={true}
+                    />
+                </div>
+            </div>
+        </div>
     );
 }
 export default Collections;
